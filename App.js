0ed771b96016c2236052ba4e826d734237456b11
aramay <kasper_341@hotmail.com>
2021-06-10 14:53:48 -0700

    add component - LandingPage

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import LandingPage from './FE/LandingPage'
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 const App = () => {
   return (
-    <LandingPage />
+    <Container className='p-3'>
+      <Jumbotron>
+        <h1 className='header'>Flash Cards - Coders</h1>
+        <LandingPage />
+      </Jumbotron>
+    </Container>
+
   )
 }
 
 export default App
+
+
