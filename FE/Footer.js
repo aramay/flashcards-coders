da9725bcedb00c2d6cb53a0d3fe4d279af171372
aramay <kasper_341@hotmail.com>
2021-06-10 19:18:23 -0700

    update css - make it stick to bottom - view port

--- FE/Footer.js
+++ FE/Footer.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 
 const Footer = () => {
   return (
-    <Container className="postion-absolute bottom-0 end-0" >
-      <Row>
-        <Col lg={true}>
-          Footer..!!
-        </Col>
-      </Row>
-    </Container>
+    <footer className='footer'>
+      Footer..!!
+    </footer>
   )
 }
-
 
 export default Footer
