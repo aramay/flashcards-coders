2ff3ec95b2632614c0cef6cbffcf209635f1e724
aramay <kasper_341@hotmail.com>
2021-06-12 10:55:30 -0700

    fix formatting

--- FE/LandingPage.js
+++ FE/LandingPage.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 
 const LandingPage = () => {
