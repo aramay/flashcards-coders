29fae8fa25029060e76e2b7bf88a4ee8ea67936c
aramay <kasper_341@hotmail.com>
2021-06-12 10:58:46 -0700

    fix formatting

--- store/store.js
+++ store/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
-import decksReducer from '../FE/decksSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import decksReducer from '../FE/decksSlice';
 
 export default configureStore({
   reducer: {
     decks: decksReducer
   }
-})
+});
