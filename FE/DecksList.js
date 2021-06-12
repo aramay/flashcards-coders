192ae1c139220089b39ba2381dd849ee05ca0d1c
aramay <kasper_341@hotmail.com>
2021-06-12 13:32:07 -0700

    DecksList component - baseline complete

--- FE/DecksList.js
+++ FE/DecksList.js
@@ -17,22 +17,33 @@ const DecksList = () => {
         <Card key={deck.id}>
           <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
           <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
+            <Card.Title>{deck.title}</Card.Title>
+            <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card`&apos`s content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroupItem>Cras justo odio</ListGroupItem>
-        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-        <ListGroupItem>Vestibulum at eros</ListGroupItem>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
+            </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>Total Cards : {deck.cards}</ListGroupItem>
+            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
+      </Col >
+    </>
+
+  ));
+
+  return (
+    <Container className='p-3'>
+      <Row>
+        {renderDecks}
+      </Row>
+    </Container>
   );
 };
   
