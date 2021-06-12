ecd007e1d3b5520b6b05a9735823424f91591444
aramay <kasper_341@hotmail.com>
2021-06-12 13:26:46 -0700

    import state from store - render decks

--- FE/DecksList.js
+++ FE/DecksList.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
 const DecksList = () => {
-  return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-      <Card.Body>
+  // state.decks.decks - comes from store prop - decks
+  const decks = useSelector(state => state.decks.decks);
+
+  const renderDecks = decks.map( deck => (
+    <>
+      <Col xs={2} md={3} lg={6} >
+        <Card key={deck.id}>
+          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+          <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
