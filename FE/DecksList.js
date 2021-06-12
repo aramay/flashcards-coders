49b041090ce7feafa28b252b7b15dd32db7860d6
aramay <kasper_341@hotmail.com>
2021-06-12 15:22:31 -0700

    refactor DecksList to show new Rows

--- FE/DecksList.js
+++ FE/DecksList.js
@@ -3,45 +3,39 @@ import { useSelector } from 'react-redux';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
+import {Col, Row, Container} from 'react-bootstrap';
 
 const DecksList = () => {
   // state.decks.decks - comes from store prop - decks
   const decks = useSelector(state => state.decks.decks);
 
-  const renderDecks = decks.map( deck => (
-    <>
-      <Col xs={2} md={3} lg={6} >
-        <Card key={deck.id}>
-          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-          <Card.Body>
-            <Card.Title>{deck.title}</Card.Title>
-            <Card.Text>
-              {deck.description}
-            </Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroupItem>Total Cards : {deck.totalCards}</ListGroupItem>
-            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-            <ListGroupItem>Vestibulum at eros</ListGroupItem>
-          </ListGroup>
-          <Card.Body>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-          </Card.Body>
-        </Card>
-      </Col >
-    </>
-
-  ));
-
+  // const renderDecks = decks.map( deck => (
   return (
     <Container className='p-3'>
-      <Row>
-        {renderDecks}
-      </Row>
+      <Row xs={1} md={2} className="g-4">
+        {decks.map( deck => (
+          <Col xs={4} md={4} lg={6} key={deck.id}>
+            <Card >
+              <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+              <Card.Body>
+                <Card.Title>{deck.title}</Card.Title>
+                <Card.Text>
+                  {deck.description}
+                </Card.Text>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroupItem>
+              Total Cards : {deck.totalCards}
+                </ListGroupItem>
+              </ListGroup>
+              <Card.Body>
+                <Card.Link href="#">Learn</Card.Link>
+                <Card.Link href="#">Unlearn</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col >
+        ))}
+      </Row>;
     </Container>
   );
 };
