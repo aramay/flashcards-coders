9d6051a076c6d19c398af4d15569e55f890a58bc
aramay <kasper_341@hotmail.com>
2021-06-12 14:11:01 -0700

    update schema design - decks

--- FE/decksSlice.js
+++ FE/decksSlice.js
@@ -2,10 +2,10 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   decks: [
-    {id: '1', title: 'JS Interview Questions', cards: '20'},
-    {id: '2', title: 'Linked List Questions', cards: '10'},
-    {id: '3', title: 'React Questions', cards: '10'},
-    {id: '4', title: 'BST Questions', cards: '10'},
+    {id: '1', title: 'JS Interview Questions', description: 'Some quick example text to build on the card title and make up the bulk of the card`&apos`s content.', totalCards: '20'},
+    {id: '2', title: 'Linked List Questions', description: 'Some quick example text to build on the card title and make up the bulk of the card`&apos`s content.', totalCards: '10'},
+    {id: '3', title: 'React Questions', description: 'Some quick example text to build on the card title and make up the bulk of the card`&apos`s content.', totalCards: '10'},
+    {id: '4', title: 'BST Questions', description: 'Some quick example text to build on the card title and make up the bulk of the card`&apos`s content.', totalCards: '10'},
   ],
   status: 'idle',
   error: null
