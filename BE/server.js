07fc1b14ca9e9a85f04dc29f0ccff829c6394af5
aramay <kasper_341@hotmail.com>
2021-06-14 15:24:36 -0700

    add server starter code

---
+++ BE/server.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+
+const app  = express();
+
+const PORT = 3000;
+
+
+// handle parding request body
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// handle request for static sites
+app.use(express.static(path.resolve(__dirname, '../client')));
+
+// catch unkown request to server
+app.use('*', (req,res) => {
+  res.send('Unkown request to server');
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}..`);
+});
+
+module.exports = app;
