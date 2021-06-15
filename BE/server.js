b8362fd5cb04837cf17ec6808542ce17d5e51a47
aramay <kasper_341@hotmail.com>
2021-06-14 18:44:04 -0700

    add route /decks for FE to get Decks list

--- BE/server.js
+++ BE/server.js
@@ -13,6 +13,12 @@ app.use(express.urlencoded({extended: true}));
 // handle request for static sites
 app.use(express.static(path.resolve(__dirname, '../client')));
 
+
+// handle get request to decks
+app.get('/decks', (req, res) => {
+  res.status(200).send('/deck GET request');
+});
+
 // catch unkown request to server
 app.use('*', (req,res) => {
   res.send('Unkown request to server');
