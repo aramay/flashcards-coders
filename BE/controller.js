bacfc0d6d634528199a2ca5fc9f103b54669e4a4
aramay <kasper_341@hotmail.com>
2021-06-14 22:57:26 -0700

    get data from DB - decks and cards

--- BE/controller.js
+++ BE/controller.js
@@ -4,9 +4,27 @@ const db = require('./models');
 
 const decksCtrl = {};
 
-decksCtrl.getDecks = (req, res, next) => {
+decksCtrl.getDecks = async (req, res, next) => {
   console.log(' getDecks ctrl ');
-  res.status(200).send('/deck GET request');
+  const queryDecks = 'select * from decks';
+  const queryCards = 'select * from cards';
+
+  try {
+    const { rows } = await db.query(queryDecks);
+    const cardRows = await db.query(queryCards);
+
+    res.locals = rows.map(deck => ({
+      ...deck,
+      cards: cardRows.rows.filter( card => card.deck_id === deck.deck_id)
+    }));
+
+    res.send({decks: res.locals});
+
+  } catch( err) {
+    return next({
+      mesg: `getDecks ctrl failed ${err}`
+    });
+  }
 };
 
 module.exports = decksCtrl;
