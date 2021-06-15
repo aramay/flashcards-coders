23b55fa311861ec7ff22c90cbc045638d8fe6939
aramay <kasper_341@hotmail.com>
2021-06-14 19:26:43 -0700

    add DB connection object

---
+++ BE/models.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+const PG_URI = '';
+
+// create a connection pool
+const pool = new Pool({
+  connectionString: PG_URI
+});
+
+// export and object : query
+// query is a function that returns the invocation of pool.query() after logging the query
+// export this object : query 
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query ', text);
+    console.log('executed params ', params);
+    return pool.query(text, params, callback);
+  }
+}
