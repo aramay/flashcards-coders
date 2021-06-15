7d36a58c9e033419c53ed5cbfeed1d4e3b664db4
aramay <kasper_341@hotmail.com>
2021-06-14 19:35:38 -0700

    add ctrl - getDecks

--- BE/controller.js
+++ BE/controller.js
@@ -6,4 +6,7 @@ const decksCtrl = {};
 
 decksCtrl.getDecks = (req, res, next) => {
   console.log(' getDecks ctrl ');
-}
+  res.status(200).send('/deck GET request');
+};
+
+module.exports = decksCtrl;
