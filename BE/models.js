aff886aa93e7423d463646a35344f0d736bf9e55
aramay <kasper_341@hotmail.com>
2021-06-17 12:40:17 -0700

    add experimental code to listen for DB events

--- BE/models.js
+++ BE/models.js
@@ -19,3 +19,35 @@ module.exports = {
     return pool.query(text, params, callback);
   }
 };
+
+// pool.on('error', (err, client) => {
+//   console.error('Unexpected error on idle client', err);
+//   process.exit(-1);
+// });
+
+// // callback - checkout a client
+// pool.connect((err, client, done) => {
+//   if (err) throw err;
+//   client.query('LISTEN addedrecord');
+
+//   client.on('notification', function(data){
+//     console.log('inside notification ');
+//     console.log(data);
+//   });
+// });
+
+/**IGNORE BELOW */
+// const client = new Client();
+
+// var query = client.query("LISTEN addedrecord");
+// query.then(res => console.log("res ", res));
+
+// pool.connect(function(err, client) {
+//   if(err) {
+//     console.log(`Error ${err}`);
+//   }
+//   client.on('notification', function(msg) {
+//     console.log(`mesage ${msg}`);
+//   });
+//   var query = pool.query("LISTEN addedrecord");
+// });
