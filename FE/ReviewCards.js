ad8205c8c83bde4f191597c92015df912b497723
aramay <kasper_341@hotmail.com>
2021-06-18 08:37:37 -0700

    add function - cycle through cards to review

--- FE/ReviewCards.js
+++ FE/ReviewCards.js
@@ -1,36 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import {Col, Row, Container, Button, Spinner} from 'react-bootstrap';
 import { addCardToReview } from './decksSlice';
 import { cardsToReview } from './decksSlice';
 import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol, MDBBtn } from 'mdb-react-ui-kit';
+import getStoredState from 'redux-persist/es/getStoredState';
+// import { reviewCards } from './decksSlice';
+import { reviewCardsCount } from './decksSlice';
 
 let counter = 0;
 
-const nextQuestion = () => {
-  console.log('button clicked ', counter ++);
-};
-
-const NextBtn = () => {
-  return (
-    <Button
-      onClick={nextQuestion}
-    >
-      Next
-    </Button>
-  );
-};
-
-const ReviewCardsList = () => {
+const ReviewCardsList = ({reviews, onNextQuestion, counter}) => {
   // console.log('CardList ', card);
   return (
     <MDBRow className='padding-btm'>
       <MDBCol sm='5'>
         <MDBCard>
           <MDBCardBody>
-            <MDBCardTitle>Question </MDBCardTitle>
+            <MDBCardTitle>Question {reviews[counter].card_id}</MDBCardTitle>
             <MDBCardText>
-              Question
+              {reviews[counter].question}
             </MDBCardText>
           </MDBCardBody>
           
@@ -42,25 +31,56 @@ const ReviewCardsList = () => {
           <MDBCardBody>
             <MDBCardTitle>Answer</MDBCardTitle>
             <MDBCardText>
-              Answer
+              {reviews[counter].answer}
             </MDBCardText>
           </MDBCardBody>
         </MDBCard>
       </MDBCol>
-      <NextBtn sm='2' />
+      <Button
+        onClick={() => onNextQuestion(reviews[counter].card_id)}
+        sm='2' >
+      Next
+      </Button>
     </MDBRow>
   );
 };
 
 export const ReviewCards = () => {
-  
 
-  const reviews = useSelector(state => cardsToReview(state));
+  const reviewCount = useSelector(state => 
+    reviewCardsCount(state)
+  );
+
+  let [reviewCCount, setReviewCCount] = useState(reviewCount);
+  console.log('reviewCCount ', reviewCCount);
+  console.log('setReviewCCount ', setReviewCCount);
+
+
+  
+  const initReviews = useSelector(state => cardsToReview(state));
+  
+  const [reviews, setReviews] = useState(initReviews);
+  
   console.log('ReviewCards ', reviews);
+
   let content ;
 
+  const handleNextQuestion = (index) => {
+    console.log('counter before click ', counter);
+    console.log('params', index);
+    // const newReviewList = reviews.splice(index, 1);
+    const newReviewList = reviews.filter( review => review.card_id !== index);
+    counter ++;
+    console.log('counter after click ', counter);
+    setReviews(newReviewList);
+
+    const newReviewCount = -- reviewCCount;
+    console.log('reviewCCount after --', newReviewCount);
+    setReviewCCount(newReviewCount);
+  };
+
   if (counter < reviews.length) {
-    content = <ReviewCardsList />;
+    content = <ReviewCardsList reviews={reviews} onNextQuestion={handleNextQuestion} counter={counter}/>;
   } else {
     content = <div>No cards to review</div>;
   }
