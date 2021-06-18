f967e370162753a296875de53d73d4af71b13821
aramay <kasper_341@hotmail.com>
2021-06-17 18:41:05 -0700

    add components and click handler

--- FE/ReviewCards.js
+++ FE/ReviewCards.js
@@ -1,24 +1,74 @@
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import {Col, Row, Container, Button, Spinner} from 'react-bootstrap';
 import { addCardToReview } from './decksSlice';
+import { cardsToReview } from './decksSlice';
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol, MDBBtn } from 'mdb-react-ui-kit';
 
-const ReviewCards = () => {
+let counter = 0;
 
-  // useEffect( () => {
-  //   dispatchEvent(addCardToReview())
-  // }, [])
+const nextQuestion = () => {
+  console.log('button clicked ', counter ++);
+};
 
-  const addCardToReviewClicked = () => {
-    dispatchEvent(addCardToReview());
-  };
-
+const NextBtn = () => {
   return (
     <Button
-      variant="primary"
-      onClick={addCardToReviewClicked}
+      onClick={nextQuestion}
     >
+      Next
     </Button>
   );
 };
 
-export default ReviewCards;
+const ReviewCardsList = () => {
+  // console.log('CardList ', card);
+  return (
+    <MDBRow className='padding-btm'>
+      <MDBCol sm='5'>
+        <MDBCard>
+          <MDBCardBody>
+            <MDBCardTitle>Question </MDBCardTitle>
+            <MDBCardText>
+              Question
+            </MDBCardText>
+          </MDBCardBody>
+          
+        </MDBCard>
+      </MDBCol>
+
+      <MDBCol sm='5'>
+        <MDBCard>
+          <MDBCardBody>
+            <MDBCardTitle>Answer</MDBCardTitle>
+            <MDBCardText>
+              Answer
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+      <NextBtn sm='2' />
+    </MDBRow>
+  );
+};
+
+export const ReviewCards = () => {
+  
+
+  const reviews = useSelector(state => cardsToReview(state));
+  console.log('ReviewCards ', reviews);
+  let content ;
+
+  if (counter < reviews.length) {
+    content = <ReviewCardsList />;
+  } else {
+    content = <div>No cards to review</div>;
+  }
+  return (
+    <Container>
+      {content}
+    </Container>
+  );
+};
+
+// export default ReviewCards;
